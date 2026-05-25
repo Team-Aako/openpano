98cdcfbdf88c41d497692737d287f32faef0793b
Yasushi Oh <calen0909@hotmail.com>
2026-05-25 16:24:43 -0400

    Initial Commit

---
+++ openpano/openpanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  openpanoApp.swift
+//  openpano
+//
+//  Created by ANZHI WENG on 2026/5/25.
+//
+
+import SwiftUI
+
+@main
+struct openpanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
